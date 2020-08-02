0e11b511cb44f75967c5c97677e703b67fe4dbf6
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2020-08-02 15:33:31 -0500

    added initial user account support

--- client/App.svelte
+++ client/App.svelte
@@ -1,49 +1,84 @@
 <script>
+  import {Meteor} from 'meteor/meteor';
   import {useTracker} from 'meteor/rdb:svelte-meteor-data';
+  import {BlazeTemplate} from 'meteor/svelte:blaze-integration';
   import {Tasks} from '../imports/tasks.js';
   import Task from './Task.svelte';
 
   let hideCompleted = false;
   let text = '';
+  let user;
+
+  // user is a store
+  $: user = useTracker(() => Meteor.user());
 
   const query = {};
   const projection = {sort: {createdAt: -1}};
+  // tasks is a store
   $: tasks = useTracker(() => Tasks.find(query, projection).fetch());
+
   $: remaining = $tasks.filter(t => !t.done).length;
 
   function addTodo() {
-    Tasks.insert({text, createdAt: new Date()});
+    Tasks.insert({
+      text,
+      createdAt: new Date(),
+      owner: Meteor.userId(),
+      username: $user.username
+    });
     text = '';
   }
 </script>
 
 <div class="container">
+  <BlazeTemplate template="loginButtons" />
+
   <header>
-    <h1>Todo List ({remaining} of {$tasks.length} remaining)</h1>
+    <h1>Todo App</h1>
   </header>
 
-  <form on:submit|preventDefault={addTodo}>
-    <input placeholder="todo text" bind:value={text} />
-    <button>Add</button>
+  {#if $user}
+    <p>{remaining} of {$tasks.length} remaining</p>
 
-    <label className="hide-completed">
-      <input type="checkbox" bind:checked={hideCompleted} />
-      Hide Completed Tasks
-    </label>
-  </form>
+    <form on:submit|preventDefault={addTodo}>
+      <input placeholder="todo text" bind:value={text} />
+      <button>Add</button>
 
-  <ul>
-    {#each $tasks as task}
-      {#if !hideCompleted || !task.done}
-        <Task {task} />
-      {/if}
-    {/each}
-  </ul>
+      <label className="hide-completed">
+        <input type="checkbox" bind:checked={hideCompleted} />
+        Hide Completed Tasks
+      </label>
+    </form>
+
+    <ul>
+      {#each $tasks as task}
+        {#if !hideCompleted || !task.done}
+          <Task {task} />
+        {/if}
+      {/each}
+    </ul>
+  {/if}
 </div>
 
 <style>
   form {
     margin-top: 0;
     padding: 1rem;
+  }
+
+  :global(#login-buttons) {
+    padding: 0.5rem;
+    text-align: right;
+  }
+
+  :global(#login-buttons a) {
+    text-decoration: none;
+  }
+
+  :global(#login-dropdown-list) {
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
   }
 </style>
--- client/main.js
+++ client/main.js
@@ -1,5 +1,6 @@
 import {Meteor} from 'meteor/meteor';
 import App from './App.svelte';
+import '../imports/accounts-config.js';
 
 Meteor.startup(() => {
   new App({target: document.getElementById('app')});
