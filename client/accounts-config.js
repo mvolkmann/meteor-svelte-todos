8df4333fdb3d4f72a0d48850e8d7adc121d72ab9
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2020-08-06 12:50:40 -0500

    another attempt at password validation

--- client/accounts-config.js
+++ client/accounts-config.js
@@ -1,5 +1,18 @@
 import {Accounts} from 'meteor/accounts-base';
+//import {AccountsTemplates} from 'meteor/useraccounts:core';
 
 Accounts.ui.config({
   passwordSignupFields: 'USERNAME_AND_EMAIL'
 });
+
+/*
+AccountsTemplates.removeField('password');
+AccountsTemplates.addField({
+  _id: 'password',
+  type: 'password',
+  required: true,
+  minLength: 6,
+  re: /(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/,
+  errStr: 'At least 1 digit, 1 lower-case and 1 upper-case'
+});
+*/
